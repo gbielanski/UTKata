46883146f5734e11fbdd4ed737594f5b143836b4
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-06-09 11:45:37 +0200

    returnTrue_asDefault FAILS

---
+++ app/src/main/java/pl/kata/bielanski/utkata/MyAlarm.java
@@ -0,0 +1,10 @@
+package pl.kata.bielanski.utkata;
+
+/**
+ * Created by gbielanski on 2016-06-09.
+ */
+public class MyAlarm {
+	public boolean getFlag() {
+		return false;
+	}
+}
--- app/src/test/java/pl/kata/bielanski/utkata/AlarmTest.java
+++ app/src/test/java/pl/kata/bielanski/utkata/AlarmTest.java
@@ -1,4 +1,15 @@
 package pl.kata.bielanski.utkata;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class AlarmTest {
+	@Test
+	public void returnTrue_asDefault(){
+		MyAlarm myAlarm = new MyAlarm();
+		boolean flag = myAlarm.getFlag();
+		assertThat(flag, is(true));
+	}
 }
